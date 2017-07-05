4557bab88c05d6f139da6203e01955ce319600fc
DESKTOP-VK36J9H\Esgar <esga999@gmail.com>
2017-07-05 23:08:25 +0200

    Cosas que te matan

--- Desarrollo/ModernZ/Classes/GameOverScene.cpp
+++ Desarrollo/ModernZ/Classes/GameOverScene.cpp
@@ -33,20 +33,13 @@ bool GameOverScene::init()
         return false;
     }
     
-	/*Size visibleSize = Director::getInstance()->getVisibleSize();
+	Size visibleSize = Director::getInstance()->getVisibleSize();
 	Vec2 origin = Director::getInstance()->getVisibleOrigin();
 	Director::getInstance()->setDisplayStats(false);
 
 	auto backgroundSprite = Sprite::create("backgroundMenu.jpg");
 	backgroundSprite->setPosition(Point(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
 	this->addChild(backgroundSprite);
-
-	MenuItemImage *buttonPlay = MenuItemImage::create("menu/play.png", "menu/playSelec.png", CC_CALLBACK_1(GameOverScene::openMainMenu, this));
-	MenuItemImage *buttonQuit = MenuItemImage::create("menu/quit.png", "menu/quitSelec.png", CC_CALLBACK_1(GameOverScene::exitGame, this));
-
-	Menu* menuOver = Menu::create(buttonPlay, buttonQuit, NULL);
-	menuOver->alignItemsVertically();
-	this->addChild(menuOver);*/
 
     return true;
 }
--- Desarrollo/ModernZ/Classes/MyContactListener.cpp
+++ Desarrollo/ModernZ/Classes/MyContactListener.cpp
@@ -1,4 +1,5 @@
 #include "MyContactListener.h"
+#include "MainMenuScene.h"
 #include "cocostudio/CocoStudio.h"
 #include "ui/CocosGUI.h"
 #include "Player.h"
@@ -19,6 +20,16 @@ void MyContactListener::BeginContact(b2Contact* contact) {
 	{
 		contactScene->getPlayer()->setJumping(false);
 	}
+
+	if (((A == DATA_PLAYER_SENSOR1 || A == DATA_PLAYER_SENSOR2) && (B == DATA_VOID)) ||
+		((B == DATA_PLAYER_SENSOR1 || B == DATA_PLAYER_SENSOR2) && (A == DATA_VOID)))
+	{
+		CCLOG("Player choca con los pinchos");
+
+		auto scene = MainMenuScene::createScene();
+		Director::getInstance()->replaceScene(TransitionFade::create(TRANSITION_TIME, scene));
+	}
+
 }
 void MyContactListener::EndContact(b2Contact* contact) {
 	unsigned long A = (unsigned long)contact->GetFixtureA()->GetUserData();
@@ -28,6 +39,15 @@ void MyContactListener::EndContact(b2Contact* contact) {
 		((B == DATA_PLAYER_SENSOR1 || B == DATA_PLAYER_SENSOR2) && (A == DATA_PLATFORM || A == DATA_PLAYER)))
 	{
 		if(contactScene->getPlayer()->getTypePlayer() == 2) contactScene->getPlayer()->setJumping(false);
+	}
+
+	if (((A == DATA_PLAYER_SENSOR1 || A == DATA_PLAYER_SENSOR2) && (B == DATA_VOID)) ||
+		((B == DATA_PLAYER_SENSOR1 || B == DATA_PLAYER_SENSOR2) && (A == DATA_VOID)))
+	{
+		CCLOG("Player choca con los pinchos");
+
+		auto scene = MainMenuScene::createScene();
+		Director::getInstance()->replaceScene(TransitionFade::create(TRANSITION_TIME, scene));
 	}
 
 
